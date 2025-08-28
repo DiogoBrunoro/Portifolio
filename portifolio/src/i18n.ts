7a56431d9a0fcef5c0c62f03525289038795f674
Diogo C Brunoro <diogocaribebrunoro@gmail.com>
2025-08-28 14:24:15 -0300

    fix locales

--- portifolio/src/i18n.ts
+++ portifolio/src/i18n.ts
@@ -2,8 +2,8 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import pt from "./locales/pt/translation.json";
-import en from "./locales/en/translation.json";
+import pt from "../src/Locales/pt/translation.json";
+import en from "../src/Locales/en/translation.json";
 
 i18n
   .use(LanguageDetector)
